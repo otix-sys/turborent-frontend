0d6000f16466893b09e94e606e62fe42893d647a
otix-sys <nebloomofficiel@gmail.com>
2026-03-29 13:23:44 +0200

    Update page.tsx

--- turborent-frontend-vercel/app/tableau-de-bord/page.tsx
+++ turborent-frontend-vercel/app/tableau-de-bord/page.tsx
@@ -83,7 +83,7 @@ export default function DashboardPage() {
                     className={`badge ${
                       !k.doc
                         ? 'badge-red'
-                        : k.doc.status === 'valide'
+                        : k.doc?.status === 'valide'
                         ? 'badge-green'
                         : 'badge-yellow'
                     }`}
